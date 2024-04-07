3f84b767d9bcabe7723406b1c43ea09c00f9cbb3
M Bimo Bayu Bagaskara <m.bimo.bayu.bagaskara-2022@fst.unair.ac.id>
2024-04-07 20:35:49 +0700

    Memperbaiki dashboard admin

--- resources/views/pages/dashboard/sidebar.blade.php
+++ resources/views/pages/dashboard/sidebar.blade.php
@@ -1,7 +1,7 @@
 <!-- Sidebar -->
 <div class="sidebar" id="mySidebar">
     <div class="side-header">
-        <img src="{{asset('assets/images/logo.png')}}" width="120" height="120">
+        <img src="{{asset('assets/images/logo-admin.jpg')}}" width="120" height="120">
         <h5 style="margin-top:10px;">Hello, {{ Auth::user( )->name }}</h5>
     </div>
     <a href="{{route('dashboard')}}"><i class="fa fa-home"></i> Home</a>
@@ -9,5 +9,6 @@
     <a href="{{route('dashboard.donations')}}"><i class="fa fa-heart"></i> Donation</a>
     <a href="{{route('dashboard.galleries')}}"><i class="fa fa-paint-brush"></i> Gallery</a>
     <a href="{{route('dashboard.blogs')}}"><i class="fa-solid fa-blog"></i> Blog</a>
+    <a href="{{route('doLogout')}}"><i class="fa-solid fa-blog"></i> Logout</a>
     
 </div>
